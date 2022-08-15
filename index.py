95def4fa59c752c86ee29fef8e2a5a40e5c5e631
Arch0wl <annstarovoitov@gmail.com>
2022-08-15 17:46:54 -0400

    new numbers

--- index.py
+++ index.py
@@ -2,13 +2,13 @@ failed_subjects="4"
 
 print("Basic Arithmetic")
 
-a=6
-b=2
+a=20
+b=3
 
 print('Addition : ', a + b)
 print('Substraction : ', a - b)
 print('Multiplication : ', a * b)
 print('Division (float) : ', a / b)
 print('Division (floor) : ', a // b) 
-print('Modulus : ', a % b)
+print('Modulus : ', a % b) #no leftover, this reason 0
 print('Exponent : ', a ** b)
